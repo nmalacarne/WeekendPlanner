0556840683dc5de446520e8987ac76b822843d36
nmalacarne <nicholas.malacarne@gmail.com>
2014-05-09 22:40:55 -0400

    - fixed some netbeans errors - removed errant closing parenthesis in test_plugin_asset.css

--- app/Model/meal_assignment.php
+++ app/Model/meal_assignment.php
@@ -4,15 +4,6 @@ class Meal_assignment extends AppModel{
 	
 	var $name = 'Meal_assignment';
 	var $belongsTo = array('User' => array('classname' => 'Meal',
-										'foreignKey' => 'meal_id'))
-	
-	
-	);
-	
-	
-	
-	
-
+										'foreignKey' => 'meal_id'));
 }
-
 ?>
